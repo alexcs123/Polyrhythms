7a112f4e74beb4223c786e584b5a595993583ff8
Alex <alex@nder.one>
2020-02-27 00:59:01 +0200

    Code tidy

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ from math import gcd
 
 
 def squbic(n):
-    while not (n == 2 or n == 3):
-        m = n
-
+    """Determines if n is a product of only twos and/or threes"""
+    while not 2 <= n <= 3:
         if n % 2 == 0:
             n /= 2
         elif n % 3 == 0:
             n /= 3
-
-        if n == m:
+        else:
             return False
 
     return True
 
 
 if __name__ == '__main__':
-    for i in range(3, 18):
+    limit = 18
+
+    for i in range(3, limit):
         for j in range(2, i):
             if gcd(i, j) == 1:
                 if squbic(i) and squbic(j):
                     print(str(i) + ':' + str(j) + ' easy')
-                elif not squbic(i) and not squbic(j):
-                    print(str(i) + ':' + str(j) + ' hard')
-                else:
+                elif squbic(i) or squbic(j):
                     print(str(i) + ':' + str(j))
+                else:
+                    print(str(i) + ':' + str(j) + ' hard')
